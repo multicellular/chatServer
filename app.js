7cb22a4cea01b15ad420cca4921c6c71463d4330
justaworker <1438367681@qq.com>
2019-03-18 13:48:47 +0800

    new

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const Koa = require('koa')
 const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
+// const bodyparser = require('koa-bodyparser')
+const koaBody = require('koa-body')
 const session = require('koa-session-minimal')
 const MysqlStore = require('koa-mysql-session')
 const logger = require('koa-logger')
 const jwtKoa = require('koa-jwt')
+const fs = require('fs')
+const path = require('path')
 const index = require('./routes/index')
 const user = require('./routes/user')
 const blog = require('./routes/blog')
@@ -17,16 +20,36 @@ const app = new Koa()
 // error handler
 onerror(app)
 
-// middlewares
-app.use(bodyparser({
-  enableTypes: ['json', 'form', 'text']
-}))
+// middlewares  bodyparser
+// app.use(bodyparser({
+//   enableTypes: ['json', 'form', 'text']
+// }))
+
+// middlewares  bodyparser
+app.use(koaBody({
+  multipart: true,
+  formidable: {
+    uploadDir: path.join(__dirname, 'public/files/'),
+    keepExtensions: true,
+    maxFieldsSize: 10 * 1024 * 1024,
+    onFileBegin: (name, file) => {
+      const fp = path.join(__dirname, 'public/files/');
+      if (!fs.existsSync(fp)) {
+        fs.mkdirSync(fp);
+      }
+    }
+  }
+}));
+// app.use(ctx => {
+//   ctx.body = JSON.stringify(ctx.request.body);
+// });
+
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
 const urls = ['/api/user/signin', '/api/user/signup', /^\/api\/blog/];
-app.use(jwtKoa({ secret: 'my_token' , passthrough:true }).unless({ path: urls }))
+app.use(jwtKoa({ secret: 'my_token', passthrough: true }).unless({ path: urls }))
 
 app.use(views(__dirname + '/views', {
   extension: 'pug'
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('koa-router')()
+const fs = require('fs')
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
@@ -18,28 +19,56 @@ router.get('/index', async (ctx, next) => {
   }
 })
 
-router.post('/uploadImage', async (ctx, next) => {
-  let image = ctx.request.body;
-  if (!image) {
-    ctx.body = {
-      code: -1,
-      msg: 'image is null'
-    }
-    return;
-  }
-  let base64Data, dataBuffer, urlPath;
-  base64Data = image.replace(/^data:image\/\w+;base64,/, "");
-  dataBuffer = Buffer.from(base64Data, 'base64');
-  urlPath = 'images/' + Date.now() + '.png';
-  await fs.writeFile('./public/' + urlPath, dataBuffer, (err, data) => {
-    if (err) {
-      throw err;
-    }
-  });
+// router.post('/uploadImage', async (ctx, next) => {
+//   let {image} = ctx.request.body;
+//   if (!image) {
+//     ctx.body = {
+//       code: -1,
+//       msg: 'image is null'
+//     }
+//     return;
+//   }
+//   let base64Data, dataBuffer, urlPath;
+//   base64Data = image.replace(/^data:image\/\w+;base64,/, "");
+//   dataBuffer = Buffer.from(base64Data, 'base64');
+//   urlPath = 'images/' + Date.now() + '.png';
+//   await fs.writeFile('./public/' + urlPath, dataBuffer, (err, data) => {
+//     if (err) {
+//       throw err;
+//     }
+//   });
+//   ctx.body = {
+//     code: 0,
+//     image: urlPath
+//   }
+// })
+
+router.post('/api/uploadFile', async (ctx, next) => {
+  
+  const file = ctx.request.files.file;
+  const url = file.path.split('public')[1];
+  file.path = url;
+  // const file = ctx.request.files.file;
+  // if (!file) {
+  //   ctx.body = {
+  //     code: -1,
+  //     msg: 'file is null'
+  //   }
+  //   return;
+  // }
+  // const render = fs.createReadStream(file.path);
+  // const filePath = './public/files/' + file.name;
+  // const upStream = fs.createWriteStream(filePath);
+  // render.pipe(upStream);
+
+  // ctx.body = {
+  //   code: 0,
+  //   image: filePath
+  // }
   ctx.body = {
     code: 0,
-    image: urlPath
-  }
+    file
+  };
 })
 
 module.exports = router
